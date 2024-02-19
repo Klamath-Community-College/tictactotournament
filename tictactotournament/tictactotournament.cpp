cad8ee87ad193ae47ae90f53989f33bf6583a126
Justin Lane <559826@students.klamathcc.edu>
2024-02-18 22:33:43 -0800

    Added GameOver and ClaimSpace functions

--- tictactotournament/tictactotournament.cpp
+++ tictactotournament/tictactotournament.cpp
@@ -8,11 +8,13 @@ using std::endl;
 
 #include <crtdbg.h>
 
+// Set board size
 const int ROW = 3;
 const int CLM = 3;
 
 void DisplayBoard(char* ptr);
-
+bool GameOver(char* ptr);
+void ClaimSpace(char token, char input, char * ptr);
 
 int main()
 {
@@ -20,12 +22,18 @@ int main()
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     // Create board[ROW][CLM] array
-    char board[ROW][CLM] = { {'0', '1', '2'}, {'3', '4', '5'}, {'6', '7', '8'} };
+    char board[ROW][CLM] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
 
     // Assign pointer to board[]
     char * board_ptr = board[0];
 
+    char tokenX = 'X';
+    char tokenO = 'O';
+    char input = '\0';
+
     DisplayBoard(board_ptr);
+    GameOver(board_ptr);
+    ClaimSpace(tokenX, input, board_ptr);
 
     return 0;
 }
@@ -69,5 +77,184 @@ void DisplayBoard(char* ptr)
         }
         
         ptr = ptr+ROW;                                                             // increment board_ptr to next row of columns
+    }
+}
+
+/******************************************************************************
+* Entry: Pointer to gameBoard array (board_ptr)
+*
+* Exit: Bool result
+*
+* Purpose: Computes if game has been won
+*
+******************************************************************************/
+bool GameOver(char* ptr)
+{
+    bool result = false;
+
+    if (ptr[0] == ptr[1] == ptr[2] || ptr[3] == ptr[4] == ptr[5] || ptr[6] == ptr[7] == ptr[8])
+    {
+        // if win is horizontal
+        result = true;
+    }
+    else if (ptr[0] == ptr[3] == ptr[6] || ptr[1] == ptr[4] == ptr[7] || ptr[2] == ptr[5] == ptr[8])
+    {
+        // if win is vertical
+        result = true;
+    }
+    else if (ptr[0] == ptr[4] == ptr[8] || ptr[2] == ptr[4] == ptr[6])
+    {
+        // if win is diagonal
+        result = true;
+    }
+
+    return result;
+}
+
+/******************************************************************************
+* Entry: char indicating player choice
+*
+* Exit: no return
+*
+* Purpose: Compares player choice to board to see if move is valid
+*
+******************************************************************************/
+void ClaimSpace(char token, char input, char * ptr)
+{
+    if (input >= '1' && input <= '9')
+    {
+        switch (input)
+        {
+            case '1':
+                if (ptr[input - 1] == '1')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '2':
+                if (ptr[input - 1] == '2')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '3':
+                if (ptr[input - 1] == '3')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '4':
+                if (ptr[input - 1] == '4')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '5':
+                if (ptr[input - 1] == '5')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '6':
+                if (ptr[input - 1] == '6')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '7':
+                if (ptr[input - 1] == '7')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '8':
+                if (ptr[input - 1] == '8')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+            case '9':
+                if (ptr[input - 1] == '9')
+                {
+                    ptr[input - 1] = token;
+                }
+                else if (ptr[input - 1] != token)
+                {
+                    // call PVP function
+                }
+                else
+                {
+                    cout << "You have already claimed this space!";
+                }
+                break;
+        }
+    }
+    else
+    {
+        cout << "Invalid choice." << endl;
     }
 }
